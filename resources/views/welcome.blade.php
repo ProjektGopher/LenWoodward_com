efbba772be151aa45a0ccce3313520114657dd5c
Len Woodward <gopher@Lens-iMac.local>
2021-05-07 21:33:48 -0700

    remove customizations from tighten application

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-        <title>Len Woodward | Tighten Application</title>
+        <title>Len Woodward{{-- | Tighten Application --}}</title>
 
         <style>
             .bubbles {
@@ -40,27 +40,7 @@
         <div class="relative overflow-hidden bg-white">
             <div x-data="{ navOpen: false }" class="relative pt-6 pb-16 sm:pb-24 lg:pb-32 bubbles">
             <x-nav />
-            {{-- <main class="px-4 mx-auto mt-16 max-w-7xl sm:mt-24 sm:px-6 lg:mt-32">
-                <div class="lg:grid lg:grid-cols-12 lg:gap-8">
-                <div class="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
-                    <h1>
-                    <span class="block mt-1 text-4xl font-extrabold tracking-tight sm:text-5xl xl:text-6xl">
-                        <span class="block text-gray-900">Hey there,</span>
-                        <span class="block text-indigo-600">Tighten</span>
-                        <span class="block text-gray-900">folks!</span>
-                    </span>
-                    </h1>
-                    <p class="mt-3 text-lg font-thin text-gray-500 sm:mt-5 sm:text-xl lg:text-xl xl:text-2xl w-80">
-                    This is the Cool Capybara, and she'd like you to meet <span class="text-indigo-600 font-extralight">Len</span>.
-                    </p>
-                    <x-svg.arrow class="hidden h-16 mx-auto mt-24 md:block arrow" />
-                </div>
-                <div class="relative mt-12 sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
-                    <x-svg.capybara class="h-64 mx-auto text-indigo-600 fill-current md:ml-0 md:h-96 xl:h-128" /><!-- I bought this SVG from Etsy for $4 :) -->
-                    <x-svg.arrow class="block h-16 mx-auto mt-12 md:hidden arrow" />
-                </div>
-                </div>
-            </main> --}}
+            {{-- <x-tighten.hey /> --}}
             </div>
         </div>
 
@@ -184,59 +164,7 @@
             </div>
         </div>
 
-        {{-- <div class="pb-48 text-center text-gray-700 md:pb-72" style="background-color: #ffaa30;">
-            <svg class="w-full h-24 mt-24 mb-48 transform rotate-180 md:mb-72" viewBox="0 0 1440 128" fill="none" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M1440 79.6584C1410.22 98.8649 1023.24 130.442 773.857 108.956C441.094 80.2875 364.84 -70.7376 0 40.1062V128H1440V79.6584Z" fill="white"/>
-            </svg>  
-            {{ -- tighten yellow #ffaa30 -- }}
-            <a id="why" class="sr-only">Why Tighten?</a>
-            <span class="inline-block mx-auto mb-24 md:tracking-widest">
-                <span class="text-6xl font-extrabold md:text-7xl lg:text-9xl">So why</span>
-                <svg class="block w-64 my-12 text-gray-600 md:hidden" id="icon-tighten-logo--vertical" viewBox="0 0 179.76 117.34">
-                    <title>Tighten</title>
-                    <g class="icon" fill="currentColor">
-                        <path class="origin-center cls-2 animate-spin" style="transform-box: fill-box; animation-duration: 3s" d="M114.82,19.15a5.32,5.32,0,0,1,2.41,4.17v26a5.32,5.32,0,0,1-2.41,4.17l-22.53,13a5.32,5.32,0,0,1-4.82,0l-22.53-13a5.34,5.34,0,0,1-2.4-4.17v-26a5.34,5.34,0,0,1,2.4-4.17l22.53-13a5.32,5.32,0,0,1,4.82,0l22.53,13Zm-7.54,18.27V35.23a1.16,1.16,0,0,0-1.16-1.16H98.89a6.75,6.75,0,0,1-6.76-6.75V20.09A1.16,1.16,0,0,0,91,18.93H88.79a1.16,1.16,0,0,0-1.16,1.16v7.23a6.75,6.75,0,0,1-6.75,6.75H73.65a1.16,1.16,0,0,0-1.16,1.16v2.19a1.15,1.15,0,0,0,1.16,1.15h7.23a6.76,6.76,0,0,1,6.75,6.76v7.23a1.16,1.16,0,0,0,1.16,1.16H91a1.16,1.16,0,0,0,1.16-1.16V45.33a6.76,6.76,0,0,1,6.76-6.76h7.23a1.15,1.15,0,0,0,1.16-1.15Z"></path>
-                    </g>
-            
-                    <g class="text" fill="currentColor">
-                        <path class="cls-2" d="M6.42,97.76H0V92.64H18.73v5.12H12.28v19.58H6.42Z"></path>
-                        <path class="cls-2" d="M28.29,92.64h5.86v24.7H28.29Z"></path>
-                        <path class="cls-2" d="M45,112.47v-15c0-3.31,1.58-4.87,4.86-4.87H59c3.28,0,4.87,1.56,4.87,4.87v3.28H58V98.36c0-.53-.18-.67-.67-.67H51.5c-.49,0-.67.14-.67.67v13.26c0,.5.18.67.67.67h5.86c.49,0,.67-.17.67-.67v-3.45H53.87v-4.84h10v9.14c0,3.28-1.59,4.87-4.87,4.87H49.84C46.56,117.34,45,115.75,45,112.47Z"></path>
-                        <path class="cls-2" d="M74.54,92.64h5.85v9.49h7.94V92.64h5.86v24.7H88.33V107.28H80.39v10.06H74.54Z"></path>
-                        <path class="cls-2" d="M110.2,97.76h-6.42V92.64h18.73v5.12h-6.45v19.58H110.2Z"></path>
-                        <path class="cls-2" d="M132.07,92.64h17v5.08H137.93v4.48h8.92v5h-8.92v5.05h11.14v5.08h-17Z"></path>
-                        <path class="cls-2" d="M159.41,92.64h6l8.71,14.5V92.64h5.68v24.7h-5.43L165.09,102v15.31h-5.68Z"></path>
-                    </g>
-                </svg>
-                <svg class="hidden w-64 mx-4 -mt-6 text-gray-600 lg:w-96 lg:-mt-16 md:inline-block" id="icon-tighten-logo--horizontal" viewBox="0 0 260.19 66.18">
-                    <title>Tighten</title>
-                    <g class="icon" fill="currentColor">
-                        <path class="origin-center cls-2 animate-spin" style="transform-box: fill-box; animation-duration: 3s" d="M52.7,17.44a4.89,4.89,0,0,1,2.19,3.8v23.7a4.89,4.89,0,0,1-2.19,3.8L32.17,60.59a4.91,4.91,0,0,1-4.39,0L7.26,48.74a4.89,4.89,0,0,1-2.19-3.8V21.24a4.89,4.89,0,0,1,2.19-3.8L27.78,5.59a4.85,4.85,0,0,1,4.39,0L52.7,17.44ZM45.83,34.08v-2A1.06,1.06,0,0,0,44.77,31H38.18A6.15,6.15,0,0,1,32,24.89V18.3A1.07,1.07,0,0,0,31,17.24H29a1.06,1.06,0,0,0-1.05,1.06v6.59A6.15,6.15,0,0,1,21.77,31H15.19a1.06,1.06,0,0,0-1.06,1.06v2a1.06,1.06,0,0,0,1.06,1.06h6.58a6.15,6.15,0,0,1,6.16,6.15v6.59a1,1,0,0,0,1.05,1h2a1.06,1.06,0,0,0,1.06-1V41.29a6.15,6.15,0,0,1,6.15-6.15h6.59a1.06,1.06,0,0,0,1.06-1.06Z"></path>
-                    </g>
-            
-                    <g class="text" fill="currentColor">
-                        <path class="cls-5" d="M86.84,25.86H80.42V20.74H99.16v5.12H92.7V45.44H86.84Z"></path>
-                        <path class="cls-5" d="M108.71,20.74h5.86v24.7h-5.86Z"></path>
-                        <path class="cls-5" d="M125.4,40.57v-15c0-3.31,1.59-4.87,4.87-4.87h9.13c3.29,0,4.87,1.56,4.87,4.87v3.28h-5.82V26.46c0-.53-.17-.67-.67-.67h-5.85c-.5,0-.67.14-.67.67V39.72c0,.5.17.67.67.67h5.85c.5,0,.67-.17.67-.67V36.27h-4.16V31.43h10v9.14c0,3.28-1.58,4.87-4.87,4.87h-9.13C127,45.44,125.4,43.85,125.4,40.57Z"></path>
-                        <path class="cls-5" d="M155,20.74h5.86v9.49h7.93V20.74h5.86v24.7h-5.86V35.38h-7.93V45.44H155Z"></path>
-                        <path class="cls-5" d="M190.62,25.86H184.2V20.74h18.74v5.12h-6.46V45.44h-5.86Z"></path>
-                        <path class="cls-5" d="M212.49,20.74h17v5.08H218.35V30.3h8.93v5h-8.93v5H229.5v5.08h-17Z"></path>
-                        <path class="cls-5" d="M239.83,20.74h6l8.72,14.5V20.74h5.68v24.7h-5.44l-9.24-15.31V45.44h-5.68Z"></path>
-                    </g>
-                </svg>
-                <span class="text-6xl font-extrabold md:text-7xl lg:text-9xl">?</span>
-            </span>
-            <p class="w-4/5 max-w-6xl mx-auto text-lg leading-relaxed md:leading-loose font-extralight">
-                In the Laravel community, the two most respected dev houses are Spatie, and Tighten. And for good reason.
-                Your encouragement to contribute to and support open source work.
-                Your dedication to work-life balance, as well as family and mental health.
-                How you hold yourselves accountable for maintaining diversity in the workplace.
-                Your respect of other cultures, and just general social responsibility.
-                Thighten has had some of the most impressive alumni, like Adam, Caleb, and Samantha.
-                I love the sense of community that radiates outward in every direction from your company.
-                I would be exceedingly proud to say to someone at a conference I'm attending: "I work at Tighten."
-            </p>
-        </div> --}}
+        {{-- <x-tighten.why /> --}}
 
         <a name="how" class="sr-only">How to reach me</a>
         <div class="relative bg-white">
