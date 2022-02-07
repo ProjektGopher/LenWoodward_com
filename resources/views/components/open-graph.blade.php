c78c7e9cef9c87d32fb85c8e584727f6d1950dba
Len Woodward <Len@ProjektGopher.com>
2022-02-07 15:05:15 -0800

    fix open graph data

--- resources/views/components/open-graph.blade.php
+++ resources/views/components/open-graph.blade.php
@@ -1,9 +1,9 @@
-<meta property="og:title" content="Len Woodward | Tighten Application" />
+<meta property="og:title" content="Len Woodward{{--  | Tighten Application --}}" />
 <meta property="og:type" content="website" />
 <meta property="og:url" content="{{ secure_url('/') }}" />
 <meta property="og:image" content="{{ asset('img/profile.png') }}" />
 
 <meta property="og:description" 
-      content="Since that very first calculator program I wrote in VB almost 19 years ago, I've been constantly writing code as ProjektGopher Multimedia."
-      />
+  content="TALL and VILT stack developer. Available for contract. ProjektGopher Multimedia."
+/>
 <meta property="og:locale" content="en_CA" />
