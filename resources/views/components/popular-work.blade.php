d936b069b1174931de5eedbb919b6f176d39515e
Len Woodward <Len@ProjektGopher.com>
2024-03-03 14:45:33 -0800

    updates for mobile viewport

--- resources/views/components/popular-work.blade.php
+++ resources/views/components/popular-work.blade.php
@@ -4,18 +4,21 @@
       <path d="M1440 79.6584C1410.22 98.8649 1023.24 130.442 773.857 108.956C441.094 80.2875 364.84 -70.7376 0 40.1062V128H1440V79.6584Z" fill="currentcolor"/>
   </svg>  
   {{-- tighten yellow #ffaa30 --}}
-  <span class="inline-block mx-auto mb-24 md:tracking-widest">
-      <span class="text-4xl font-bold md:text-5xl lg:text-6xl">Wanna checkout <span class="text-gray-500">(pun intended)</span>
-      <br />some of my more popular work?</span>
+  <span class="inline-block mx-auto mb-24 md:tracking-widest max-w-xs sm:max-w-5xl">
+      <span class="text-4xl font-bold md:text-5xl lg:text-6xl">
+        Wanna checkout
+        <span class="text-gray-500">(pun intended)</span>
+        some of my more popular work?
+      </span>
   </span>
-  <div class="w-4/5 max-w-7xl mx-auto grid grid-cols-2 p-8 gap-8">
+  <div class="w-4/5 max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 p-4 sm:p-8 gap-8">
     <a href="https://github.com/ProjektGopher/whisky">
-      <article class="bg-white h-full rounded-2xl shadow-2xl p-8 mt-12 text-lg text-gray-700 leading-relaxed md:leading-loose font-extralight flex flex-col justify-between hover:scale-105 transform transition-all">
+      <article class="bg-white h-full rounded-2xl shadow-2xl p-4 sm:p-8 mt-12 text-lg text-gray-700 leading-relaxed md:leading-loose font-extralight flex flex-col justify-between hover:scale-105 transform transition-all">
         <img src="https://github.com/ProjektGopher/whisky/blob/main/art/logo.svg?raw=true" alt="whisky logo" class="w-full mx-auto" />
         <p class="w-4/5 mx-auto">
           Whisky is the simplest, framework agnostic, CLI tool for managing and enforcing a php project's git hooks across an entire team.
         </p>
-        <ul class="flex justify-between w-4/5 mx-auto mt-12 text-base text-gray-500">
+        <ul class="flex flex-col sm:flex-row space-y-4 sm:space-y-0 justify-between w-4/5 mx-auto mt-12 text-base text-gray-500">
           <li class="flex gap-2 content-center justify-start"><x-svg.tag />v0.6.0</li>
           <li class="flex gap-2 content-center justify-start"><x-svg.star />128</li>
           <li class="flex gap-2 content-center justify-start"><x-svg.download />14k downloads</li>
@@ -24,7 +27,7 @@
     </a>
 
     <a href="https://github.com/ProjektGopher/laravel-ffmpeg-tools" target="_blank">
-      <article class="bg-white h-full rounded-2xl shadow-2xl p-8 mt-12 text-lg text-gray-700 leading-relaxed md:leading-loose font-extralight flex flex-col justify-between hover:scale-105 transform transition-all">
+      <article class="bg-white h-full rounded-2xl shadow-2xl p-4 sm:p-8 mt-12 text-lg text-gray-700 leading-relaxed md:leading-loose font-extralight flex flex-col justify-between hover:scale-105 transform transition-all">
         <span class="text-5xl font-serif text-black mt-8">Laravel FFMpeg Tools</span>
         <div class="w-4/5 opacity-30 flex justify-around mx-auto">
           <x-logos.laravel class="h-32" />
@@ -33,7 +36,7 @@
         <p class="w-4/5 mx-auto">
           Tools and utilities to help generate complex strings for FFMpeg in Laravel.
         </p>
-        <ul class="flex justify-between w-4/5 mx-auto mt-12 text-base text-gray-500">
+        <ul class="flex flex-col sm:flex-row space-y-4 sm:space-y-0 justify-between w-4/5 mx-auto mt-12 text-base text-gray-500">
           <li class="flex gap-2 content-center justify-start"><x-svg.tag />v0.7.0</li>
           <li class="flex gap-2 content-center justify-start"><x-svg.star />44</li>
           <li class="flex gap-2 content-center justify-start"><x-svg.download />6.3k downloads</li>
