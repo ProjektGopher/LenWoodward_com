a8bc77651bca23d38984b8340ae00394c814b12f
Len Woodward <Len@ProjektGopher.com>
2024-03-03 17:32:05 -0800

    add media mentions section

---
+++ resources/views/components/media-mentions.blade.php
@@ -0,0 +1,63 @@
+<a id="media" class="sr-only">Media Mentions</a>
+<div class="-mt-32 pt-32 pb-48 text-center bg-pink-200 text-gray-700 md:pb-72">
+    <span class="inline-block mx-auto mb-24 md:tracking-widest">
+        <span class="text-4xl font-extrabold md:text-5xl lg:text-6xl">Here's some public stuff I've done</span>
+    </span>
+    <div class="w-4/5 max-w-7xl mt-12 mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 p-4 sm:p-8 gap-8">
+        <a href="https://laravel-news.com/lifecycle-hooks-in-laravel-how-to-build-them-and-why-you-d-want-to"
+            target="_blank">
+            <article
+                class="bg-white h-full pb-4 rounded-2xl shadow-2xl text-lg text-gray-700 leading-relaxed md:leading-loose font-extralight flex flex-col justify-between hover:scale-105 transform transition-all">
+                <div class="aspect-[2/1] w-full rounded-lg shadow-lg">
+                    <img src="https://picperf.io/https://laravelnews.s3.amazonaws.com/images/lifecycle-hooks-featured.png"
+                        alt="Lifecycle hooks in Laravel - How to build them, and why you'd want to image"
+                        class="h-full w-full rounded-lg object-cover" loading="lazy">
+                </div>
+                <h3 class="mt-4 text-xl font-bold sm:text-2xl px-4">
+                    Lifecycle hooks in Laravel - How to build them, and why you'd want to
+                </h3>
+                <p class="mt-3 max-w-2xl text-gray-600 px-4">
+                    We, as programmers, have to break complicated problems down into smaller chunks. Sometimes we
+                    have to interact with this code differently based on som...
+                </p>
+            </article>
+        </a>
+
+        <a href="https://laravel-news.com/laravel-ffmpeg-tools" target="_blank">
+            <article
+                class="bg-white h-full pb-4 rounded-2xl shadow-2xl text-lg text-gray-700 leading-relaxed md:leading-loose font-extralight flex flex-col justify-between hover:scale-105 transform transition-all">
+                <div class="aspect-[2/1] w-full rounded-lg shadow-lg">
+                    <img src="https://picperf.io/https://laravelnews.s3.amazonaws.com/images/tween-featured-image.jpg"
+                        alt="Laravel FFMpeg tools image" class="h-full w-full rounded-lg object-cover" loading="lazy">
+                </div>
+                <h3 class="mt-4 text-xl font-bold sm:text-2xl px-4">
+                    Laravel FFMpeg tools
+                </h3>
+                <p class="mt-3 max-w-2xl text-gray-600 px-4">
+                    This package allows you to build FFMpeg strings in a fluent, easily maintainable way that feels
+                    familiar to php and Laravel devs.
+                </p>
+            </article>
+        </a>
+
+        <a href="https://www.northmeetssouth.audio/139" target="_blank">
+            <article
+                class="bg-white h-full pb-4 rounded-2xl shadow-2xl text-lg text-gray-700 leading-relaxed md:leading-loose font-extralight flex flex-col justify-between hover:scale-105 transform transition-all">
+                <div class="aspect-square w-full rounded-lg shadow-lg">
+                    <img src="https://images.transistor.fm/images/show/4883/medium_1572777036-artwork.jpg"
+                        alt="North Meets South podcast" class="h-full w-full rounded-lg object-cover">
+                </div>
+                <section class="relative p-4">
+                    <h3 class="text-gray-500 text-lg mb-4 font-extralight flex items-center">
+                        Episode 139 (<time>June 27, 2023</time>)
+                    </h3>
+                    <span class="absolute text-gray-400 text-sm flex top-4 right-4">
+                        <x-svg.clock class="h-4 w-4" />
+                        <span>41:53</span>
+                    </span>
+                    <h2 class="text-gray-700 text-xl font-extralight">Sipping Whisk(e)y with Len Woodward</h2>
+                </section>
+            </article>
+        </a>
+    </div>
+</div>
