557a2af3988925acb973ba95437721a98830a847
Len Woodward <Len@ProjektGopher.com>
2024-04-05 15:45:50 -0700

    add hey and why components for laravel

---
+++ resources/views/components/laravel/why.blade.php
@@ -0,0 +1,21 @@
+<div class="pb-48 text-center text-gray-50 from-red-500 to-red-600 bg-gradient-to-br md:pb-72">
+    <svg class="w-full h-24 mt-24 mb-48 transform rotate-180 md:mb-72" viewBox="0 0 1440 128" fill="none" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1440 79.6584C1410.22 98.8649 1023.24 130.442 773.857 108.956C441.094 80.2875 364.84 -70.7376 0 40.1062V128H1440V79.6584Z" fill="white"/>
+    </svg>  
+    <a id="why" class="sr-only">Why Laravel?</a>
+    <span class="inline-block mx-auto mb-24 md:tracking-widest">
+        <span class="text-6xl font-extrabold md:text-7xl lg:text-9xl">
+            So why <span class="border-b-2 border-red-300">Laravel</span>?
+        </span>
+    </span>
+    <p class="w-4/5 max-w-6xl mx-auto text-xl leading-relaxed md:leading-loose font-light">
+        It's not an exaggeration to say that Laravel has changed my life.
+        I appreciate your encouragement to contribute to and support open source work, as well as
+        your dedication to work-life balance, and family/mental health.
+        I admire how you hold yourselves accountable for maintaining diversity in the workplace,
+        your respect of other cultures, and just general social responsibility.
+        Laravel has some of the most impressive minds; Like Jess, Tim, and Joe.
+        I love the sense of community that radiates outward in every direction from your company.
+        I would be exceedingly proud to say to someone at a conference I'm attending: "I work at Laravel."
+    </p>
+</div>
