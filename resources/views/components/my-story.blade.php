42f58194125cbccc70be4f8b29130bc894988b8b
Len Woodward <Len@ProjektGopher.com>
2024-08-10 14:59:00 -0700

    componentize my-story

---
+++ resources/views/components/my-story.blade.php
@@ -0,0 +1,46 @@
+<div class="w-full space-y-8">
+  <p class="">
+      <a href="https://penhome.ca" target="_blank">
+          <div class="relative inline mr-2">
+              <div class="absolute top-0 left-0 w-full h-full transform bg-pink-200 rotate-1"></div>
+              <span class="relative p-1 -mr-2 text-gray-800 font-md hover:underline">PenHome Construction Services</span>
+          </div>
+      </a>
+      was my primary source of income for 15 years. My programming journey there started predictably enough: simply
+      creating and maintaining a marketing website for a family run construction company using HTML and CSS. After
+      several iterations over the years, I eventually rebuilt the site using Wordpress, as many do. As the needs of
+      the company grew, so did my skills. I learned PHP, MySQL, and JS; I also discovered Laravel. Over time, Laravel
+      became the backbone of a custom CRM I built for the company. PenHome ultimately closed shortly after Covid.
+  </p>
+  <p class="">
+      <a href="https://projektgopher.com" target="_blank">
+          <div class="relative inline mr-2">
+              <div class="absolute top-0 left-0 w-full h-full transform bg-yellow-200 -rotate-1"></div>
+              <span class="relative p-1 -mr-2 text-gray-800 font-md hover:underline">ProjektGopher Multimedia</span>
+          </div>
+      </a>
+      has been the umbrella under which I've constantly written code for personal projects, and for freelance projects, 
+      since that very first calculator program I wrote in VB over {{ now()->format('Y') - 2003 }} years ago. It became my
+      full-time job in 2021, and it's been exceedingly fulfilling. I work exclusively in the web, and am always staying
+      up&#x2011;to&#x2011;date with current trends as well as new and interesting technologies.
+  </p>
+  <p class="">
+      <a href="https://artisan.build" target="_blank">
+          <div class="relative inline mr-2">
+              <div class="absolute top-0 left-0 w-full h-full transform bg-green-200 rotate-1"></div>
+              <span class="relative p-1 -mr-2 text-gray-800 font-md hover:underline">Artisan Build</span>
+          </div>
+      </a>
+      is a brand new partnership born out of a highly compatible working relationship with a friend and colleague from
+      a client project. Together we're building a small suite of narrowly targeted tools for Laravel developers, as
+      well as co-founding some interesting projects with outside partners who are better at marketing than we are.
+      We're both excited to see where this new venture takes us. We also have a podcast, and a
+      <a href="https://YouTube.com/@ArtisanBuild" target="_blank">
+          <div class="relative inline mr-2 bg-indigo-100">
+              <div class="absolute top-0 left-0 w-full h-full transform bg-purple-200 rotate-1"></div>
+              <span class="relative p-1 -mr-2 text-gray-800 font-md hover:underline">YouTube&nbsp;channel.</span>
+          </div>
+      </a>
+      {{-- We don't generally take on much traditional client work, but we're always open to interesting opportunities. --}}
+  </p>
+</div>
