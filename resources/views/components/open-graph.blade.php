94ca1128a59075cedf9799a2ae2347843459f7ce
Len Woodward <Len@ProjektGopher.com>
2024-08-10 15:35:27 -0700

    update open graph metadata

--- resources/views/components/open-graph.blade.php
+++ resources/views/components/open-graph.blade.php
@@ -1,9 +1,14 @@
-<meta property="og:title" content="Len Woodward{{--  | Tighten Application --}}" />
+<meta property="og:title" content="Len Woodward" />
 <meta property="og:type" content="website" />
-<meta property="og:url" content="{{ secure_url('/') }}" />
-<meta property="og:image" content="{{ asset('img/profile.png') }}" />
-
-<meta property="og:description" 
-  content="TALL and VILT stack developer. Available for contract. ProjektGopher Multimedia."
-/>
 <meta property="og:locale" content="en_CA" />
+<meta property="og:url" content="{{ route('home') }}" />
+<meta property="og:description" content="Len Woodward - TALL and VILT stack web developer - Available for Contract" />
+<meta property="og:image" content="{{ asset('img/OpenGraph-1200x630.jpg')}}" />
+<meta property="og:image:secure_url" content="{{ asset('img/OpenGraph-1200x630.jpg')}}" />
+<meta property="og:image:type" content="image/jpeg" />
+<meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="630" />
+<meta property="og:image:alt" content="Len Woodward - TALL and VILT stack developer - Available for Contract" />
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:site" content="@ProjektGopher" />
+<meta name="twitter:creator" content="@ProjektGopher" />
