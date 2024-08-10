58d09c16a6026d68bd105dca4566f282bf0eac9b
Len Woodward <Len@ProjektGopher.com>
2024-08-10 14:58:08 -0700

    componentize contact card

---
+++ resources/views/components/lets-chat.blade.php
@@ -0,0 +1,23 @@
+<div class="max-w-lg mx-auto">
+    <h2 class="mb-16 text-3xl text-purple-900 md:text-6xl font-extralight tracking-loose">
+        Let's chat!
+    </h2>
+    <div class="space-y-8 whitespace-nowrap">
+        <div>
+            <x-svg.phone class="inline object-contain w-12 h-12 mr-1 -mt-4" />
+            <span class="inline text-2xl text-purple-600 md:text-3xl font-extralight"><a href="tel:6049922418">1.604.992.2418</a></span>
+        </div>
+        <div>
+            <x-svg.envelope class="inline object-contain w-12 h-12 mr-1 -mt-4" />
+            <span class="inline text-2xl text-purple-600 md:text-3xl font-extralight">Len at ProjektGopher dot com</span>
+        </div>
+        <div>
+            <x-svg.bird class="inline object-contain w-12 h-12 mr-1 -mt-4" />
+            <span class="inline text-2xl text-purple-600 md:text-3xl font-extralight">@ProjektGopher</span>
+        </div>
+        <div>
+            <x-logos.github class="inline object-contain w-12 h-12 mr-1 -mt-4 text-purple-600" />
+            <span class="inline text-2xl text-purple-600 md:text-3xl font-extralight">ProjektGopher</span>
+        </div>
+    </div>
+</div>
